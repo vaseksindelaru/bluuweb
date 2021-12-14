36090f7292385b1f949755ca26672be4f4eee0ca
vaclav sindelar <apple@vaclavs-MacBook-Pro.local>
2021-12-14 11:07:32 +0100

    bluuweb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState,Fragment} from 'react'
 
 function App() {
+  const[arrayNum,setArrayNum]= useState([1,2,3,4,5,])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+     <h2>lista</h2>
+     {arrayNum.map(item=>
+       <p>{item}</p>
+    )}
+    </Fragment>
   );
 }
 
